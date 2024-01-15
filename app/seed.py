3cbfa3f959a75810c01dff32a524ecccbe3bc206
Frankline Isaboke <isabokefrankline@gmail.com>
2024-01-15 13:55:57 +0300

    seeded data

---
+++ app/seed.py
@@ -0,0 +1,52 @@
+import random
+from app import app, db
+from models import Power, Hero, hero_powers, HeroPower
+
+app.app_context().push()
+
+print("🦸‍♀️ Seeding powers...")
+powers_data = [
+    {"name": "super strength", "description": "gives the wielder super-human strengths"},
+    {"name": "flight", "description": "gives the wielder the ability to fly through the skies at supersonic speed"},
+    {"name": "super human senses", "description": "allows the wielder to use her senses at a super-human level"},
+    {"name": "elasticity", "description": "can stretch the human body to extreme lengths"}
+]
+
+for power_data in powers_data:
+    power = Power(name=power_data["name"], description=power_data["description"])
+    db.session.add(power)
+
+print("🦸‍♀️ Seeding heroes...")
+heroes_data = [
+    {"name": "Kamala Khan", "super_name": "Ms. Marvel"},
+    {"name": "Doreen Green", "super_name": "Squirrel Girl"},
+    {"name": "Gwen Stacy", "super_name": "Spider-Gwen"},
+    {"name": "Janet Van Dyne", "super_name": "The Wasp"},
+    {"name": "Wanda Maximoff", "super_name": "Scarlet Witch"},
+    {"name": "Carol Danvers", "super_name": "Captain Marvel"},
+    {"name": "Jean Grey", "super_name": "Dark Phoenix"},
+    {"name": "Ororo Munroe", "super_name": "Storm"},
+    {"name": "Kitty Pryde", "super_name": "Shadowcat"},
+    {"name": "Elektra Natchios", "super_name": "Elektra"}
+]
+
+for hero_data in heroes_data:
+    hero = Hero(name=hero_data["name"], super_name=hero_data["super_name"])
+    db.session.add(hero)
+
+print("🦸‍♀️ Adding powers to heroes...")
+strengths = ["Strong", "Weak", "Average"]
+heroes = Hero.query.all()
+powers = Power.query.all()
+
+for hero in heroes:
+    num_powers = random.randint(1, 3)
+    for _ in range(num_powers):
+        power = random.choice(powers)
+        strength = random.choice(strengths)
+        hero_power = HeroPower(hero=hero, power=power, strength=strength)
+        db.session.add(hero_power)
+
+db.session.commit()
+
+print("🦸‍♀️ Done seeding!")
